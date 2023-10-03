2dfabe9f7221d0e388e02a493e3e268ec3d5689f
Sirio <sirioroberto1@hotmail.com>
2023-10-03 16:27:02 -0300

    Created Gift class since now items have properties (id, name, price)

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 
+let giftId = 1;
+const Gift = function (name, price) {
+  this.id = giftId++;
+  this.name = name;
+  this.price = price;
+}
 
 const itemList = [
-  "Teddy Bear",
-  "Big Red Ball",
-  "Huge Bear",
-  "Candy",
-  "Stuffed Tiger",
-  "Stuffed Dragon",
-  "Skateboard",
-  "Toy Car",
-  "Basketball",
-  "Scary Mask"
+  new Gift("Teddy Bear", 10),
+  new Gift("Big Red Ball", 5),
+  new Gift("Huge Bear", 50),
+  new Gift("Candy", 8),
+  new Gift("Stuffed Tiger", 15),
+  new Gift("Stuffed Dragon", 30),
+  new Gift("Skateboard", 100),
+  new Gift("Toy Car", 25),
+  new Gift("Basketball", 20),
+  new Gift("Scary Mask", 75),
 ];
 
 function main() {
@@ -18,7 +24,7 @@ function main() {
   console.log("Hello friend! Thank you for visiting the carnival!");
   console.log("Here's the list of gifts:\n");
 
-  itemList.forEach(item => console.log(item));
+  itemList.forEach(item => console.log(`${item.id}- ${item.name}, Cost: ${item.price} tickets`));
 }
 
 main();
